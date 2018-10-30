aca194dc14402d35e806972cdaf5c74f655b5578
Iago C <iagocrpi@gmail.com>
2018-10-30 13:49:51 +0100

    Limpiado el scope global.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const RANDOM_DOG_URL = "https://dog.ceo/api/breeds/image/random";
-
 document.querySelector(".add-doggo").addEventListener("click", onClick);
 
 function onClick(_event) {
+  const RANDOM_DOG_URL = "https://dog.ceo/api/breeds/image/random";
   fetch(RANDOM_DOG_URL)
     .then(function(apiResponse) {
       console.log(apiResponse);
