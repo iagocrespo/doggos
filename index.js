9c7f4d9f7383955f617208f0a93d2723c0d6cf61
Iago C <iagocrpi@gmail.com>
2018-10-30 14:22:49 +0100

    Reducido en funcion toJason.

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ function start() {
 
 function onClick(_event) {
   const RANDOM_DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
   fetch(RANDOM_DOG_URL)
-    .then(function(apiResponse) {
-      console.log(apiResponse);
-      return apiResponse.json();
-    })
+    .then(toJson)
     .then(function(jsonResponse) {
       const image = document.createElement("img");
       image.alt = "Perrito bonito!";
@@ -18,4 +16,8 @@ function onClick(_event) {
 
       document.querySelector(".doggos").appendChild(image);
     });
+}
+
+function toJson(apiResponse) {
+  return apiResponse.json();
 }
